34ac2c05f9e7d9fbbe4372c664014e37fb220f94
Willian <willian.alves.b15@gmail.com>
2022-11-16 20:54:31 -0300

    feat: created transactions

---
+++ backend/src/database/models/Transactions.ts
@@ -0,0 +1,33 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Accounts from './accounts';
+
+class Transactions extends Model {
+  public id!: number;
+  public debitedAccountId!: number;
+  public creditedAccountId!: number;
+  public value: number;
+  public createdAt!: Date;
+}
+
+Transactions.init({
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  debitedAccountId: DataTypes.INTEGER,
+  creditedAccountId: DataTypes.INTEGER,
+  value: DataTypes.STRING,
+  createdAt: DataTypes.DATE,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'transactions',
+  timestamps: true,
+});
+
+Transactions.hasMany(Accounts, { foreignKey: 'debitedAccountId' });
+Transactions.hasMany(Accounts, { foreignKey: 'creditedAccountId' });
+
+export default Transactions;
