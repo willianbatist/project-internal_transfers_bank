7c6103c2b9492d3603dde12441ded38eaea0782b
Willian <willian.alves.b15@gmail.com>
2022-11-16 18:05:30 -0300

    feat: created model accounts

---
+++ backend/src/database/models/accounts.ts
@@ -0,0 +1,23 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Accounts extends Model {
+  public id!: number;
+  public balance: string;
+}
+
+Accounts.init({
+  id: {
+    primaryKey: true,
+    autoIncrement: true,
+    type: DataTypes.INTEGER,
+  },
+  balance: DataTypes.STRING,
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'Accounts',
+  timestamps: false,
+});
+
+export default Accounts;
