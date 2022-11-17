6c12cc6d71d2805b5c7c5e22ad15cb9e0baec142
Willian <willian.alves.b15@gmail.com>
2022-11-16 21:39:33 -0300

    feat: created migration transactions

---
+++ backend/src/database/migrations/03-transactions.js
@@ -0,0 +1,41 @@
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('transactions', {
+        id:{
+          primaryKey: true,
+          autoIncrement: true,
+          type: Sequelize.INTEGER,
+        },
+        debitedAccountId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'accounts',
+            key: 'id'
+          },
+          onDelete: 'CASCADE',
+        },
+        creditedAccountId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'accounts',
+            key: 'id'
+          },
+          onDelete: 'CASCADE',
+        },
+        value: {
+          type: Sequelize.STRING,
+          allowNull: false,
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          defaultValue: false,
+        },
+    });
+  },
+  
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('transactions');
+  },
+  };
